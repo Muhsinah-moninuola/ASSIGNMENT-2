4f78ef05e28e56cf717bb563b89e4e99e9bee734
muhsinah moninuola <muhsinahololade@gmail.com>
2022-11-04 07:46:17 +0100

    making changes

--- movie-title.py
+++ movie-title.py
@@ -1,1 +1,7 @@
- 
+import docx
+file = docx.Document('Nafsts.docx.doc')
+for i in range(0,len(file.paragraphs),2):
+    line=file.paragraphs[i].runs[0]
+    line.text=line.text.replace('%20', ' ').replace('360p_','').replace(' .', '.')
+
+file.save('Nafsts.docx')
